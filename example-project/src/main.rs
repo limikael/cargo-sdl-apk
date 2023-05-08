2e48777e28bdf7ce741e6b48e8a8cbd8bc80cabd
Mikael Lindqvist <li.mikael@gmail.com>
2023-05-08 13:23:56 +0800

    cargo fmt

--- example-project/src/main.rs
+++ example-project/src/main.rs
@@ -1,4 +1,4 @@
-use sdl2::event::{Event};
+use sdl2::event::Event;
 
 // The entry point for our application must be caled SDL_main, and
 // must be attributed #[no_mangle]. From within this function we can
@@ -7,34 +7,33 @@ use sdl2::event::{Event};
 #[no_mangle]
 #[allow(non_snake_case)]
 pub fn SDL_main() {
-	main();
+    main();
 }
 
 pub fn main() {
-	let sdl=sdl2::init().unwrap();
-	let video_subsystem=sdl.video().unwrap();
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
 
-	let window=video_subsystem
-		.window("Testing SDL",800,600)
-		.opengl()
-		.resizable()
-		.build()
-		.unwrap();
+    let window = video_subsystem
+        .window("Testing SDL", 800, 600)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
 
-	let _gl_context=window.gl_create_context().unwrap();
-	gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let _gl_context = window.gl_create_context().unwrap();
+    gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
-	unsafe { gl::ClearColor(0.0, 0.5, 0.75, 0.0); }
-	unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
-	window.gl_swap_window();
+    unsafe {
+        gl::ClearColor(0.0, 0.5, 0.75, 0.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+    }
+    window.gl_swap_window();
 
-	let mut event_pump = sdl.event_pump().unwrap();
-
+    let mut event_pump = sdl.event_pump().unwrap();
     loop {
-    	match event_pump.wait_event() {
-            Event::Quit { .. } | Event::KeyDown { .. } => {
-            	std::process::exit(0)
-            },
+        match event_pump.wait_event() {
+            Event::Quit { .. } | Event::KeyDown { .. } => std::process::exit(0),
             _ => {}
         }
     }
